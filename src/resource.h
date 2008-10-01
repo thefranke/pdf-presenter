169e4fdf4198a20253e8a6e9b362692de56fd335
tob.franke <tob.franke@56dae35a-bf42-0410-8ddc-61b96861df82>
2008-10-01 21:26:07 +0000

    - Changed icon theme - Changed behavior of button toolbar

--- src/resource.h
+++ src/resource.h
@@ -4,16 +4,21 @@
 // Used by pdfgui.rc
 //
 #define NEXT                            102
-#define PREV                            103
-#define RUN                             104
-#define IDB_BITMAP4                     105
-#define STOP                            105
+#define NEXT_INACT                      103
+#define PREV                            104
+#define PREV_INACT                      105
+#define RUN                             106
+#define RUN_INACT                       107
+#define OPEN                            108
+#define STOP                            109
+#define HELP                            110
+#define NOTES                           111
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        112
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
